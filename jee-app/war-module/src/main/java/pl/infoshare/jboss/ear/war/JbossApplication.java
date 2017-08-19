e8c20ace64c9f40a83178085bd4d9a7db53f1f02
Milo4321 <mocmil2732@gmail.com>
2017-08-19 21:32:48 +0100

    Now WAR dependency contains both: JAX-RS service and jsp/servlet application.

--- jee-app/war-module/src/main/java/pl/infoshare/jboss/ear/war/JbossApplication.java
+++ jee-app/war-module/src/main/java/pl/infoshare/jboss/ear/war/JbossApplication.java
@@ -3,6 +3,6 @@ package pl.infoshare.jboss.ear.war;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("")
+@ApplicationPath("api")
 public class JbossApplication extends Application {
 }
